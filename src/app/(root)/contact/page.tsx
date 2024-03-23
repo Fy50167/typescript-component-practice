7a4594b53986516881a60ae988ae7126f240b1e0
Francis Yang <fy50167@gmail.com>
2024-03-23 03:05:28 -0700

    contact form

---
+++ src/app/(root)/contact/page.tsx
@@ -0,0 +1,9 @@
+import Contact from '@/components/Contact';
+
+export default function Page() {
+    return (
+        <div className='w-100 bg-black min-h-[100vh] flex justify-center items-center'>
+            <Contact />
+        </div>
+    );
+}
