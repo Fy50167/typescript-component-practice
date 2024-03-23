95fdba8966e17bb0aa8d613041a324823c175692
Francis Yang <fy50167@gmail.com>
2024-03-23 00:02:59 -0700

    tabs component

--- src/app/(root)/display/page.tsx
+++ src/app/(root)/display/page.tsx
@@ -2,8 +2,30 @@ import Tabs from '@/components/Tabs';
 
 export default function Page() {
     return (
-        <div className='container'>
-            <Tabs />
+        <div className='w-100 bg-black min-h-[100vh] flex justify-center items-center'>
+            <Tabs
+                items={[
+                    {
+                        button: 'HTML',
+                        content: `The HyperText Markup Language or HTML is the
+                        standard markup language for documents designed to
+                        be displayed in a web browser.`,
+                    },
+                    {
+                        button: 'CSS',
+                        content: `Cascading Style Sheets is a style sheet language
+                        used for describing the presentation of a document
+                        written in a markup language such as HTML or XML.`,
+                    },
+                    {
+                        button: 'JavaScript',
+                        content: `JavaScript, often abbreviated as JS, is a
+                        programming language that is one of the core
+                        technologies of the World Wide Web, alongside HTML
+                        and CSS.`,
+                    },
+                ]}
+            />
         </div>
     );
 }
--- src/components/Tabs.tsx
+++ src/components/Tabs.tsx
@@ -1,7 +1,47 @@
-import React from 'react';
+'use client';
 
-const Tabs = () => {
-    return <div></div>;
+import { useState } from 'react';
+
+interface Item {
+    button: string;
+    content: string;
+}
+
+interface Props {
+    items: Array<Item>;
+}
+
+const Tabs = ({ items }: Props) => {
+    const [currentTab, setCurrentTab] = useState<string>(items[0].button);
+
+    const setTab = (e: React.MouseEvent<HTMLButtonElement>): void => {
+        setCurrentTab(e.currentTarget.textContent || '');
+    };
+
+    return (
+        <section className='w-[90%] flex flex-col items-center justify-between py-4 px-8 bg-white rounded-md'>
+            <div className='w-full flex justify-evenly items-center'>
+                {items.map((tab) => (
+                    <div
+                        key={tab.button}
+                        className={`border-b-[1px] border-black w-1/3 mx-[3rem] flex justify-center hover:border-blue-500 duration-200 ${
+                            tab.button === currentTab ? 'border-blue-500' : ''
+                        }`}
+                    >
+                        <button onClick={setTab}>{tab.button}</button>
+                    </div>
+                ))}
+            </div>
+
+            <div className='w-full flex justify-center px-[1.5rem]'>
+                {items.map((tab, index) => (
+                    <p key={index} hidden={tab.button !== currentTab}>
+                        {tab.content}
+                    </p>
+                ))}
+            </div>
+        </section>
+    );
 };
 
 export default Tabs;
