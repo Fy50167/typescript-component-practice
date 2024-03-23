fac35334cca2fd59dda9f26be8c4fc93e903dc4f
Francis Yang <fy50167@gmail.com>
2024-03-23 04:02:08 -0700

    traffic light functionality

--- src/components/Traffic.tsx
+++ src/components/Traffic.tsx
@@ -1,3 +1,7 @@
+'use client';
+
+import { useState, useEffect } from 'react';
+
 interface Light {
     color: string;
     wait: number;
@@ -8,13 +12,24 @@ interface Props {
 }
 
 const Traffic = ({ lights }: Props) => {
+    const [currentLight, setCurrentLight] = useState<number>(0);
+
+    useEffect(() => {
+        const timeout = setTimeout(() => {
+            setCurrentLight((currentLight + 1) % lights.length);
+        }, lights[currentLight].wait);
+        return () => clearTimeout(timeout);
+    }, [currentLight]);
+
     return (
         <section className='w-[90%] flex flex-col items-center justify-between py-4 px-8 bg-white rounded-md'>
             <div className='w-1/3 h-[35rem] bg-slate-400 rounded-lg flex flex-col justify-evenly items-center'>
                 {lights.map((light, index) => (
                     <div
                         key={light.color}
-                        className='border-2 bg-black rounded-full w-40 h-40'
+                        className={`border-2 bg-black rounded-full w-40 h-40 ${
+                            currentLight === index ? `${light.color}` : ''
+                        }`}
                     ></div>
                 ))}
             </div>
