13e6975f48a67342d17f60bd510595f5bd7c3cc6
Francis Yang <fy50167@gmail.com>
2024-03-23 03:56:08 -0700

    render traffic lights

--- src/components/Traffic.tsx
+++ src/components/Traffic.tsx
@@ -8,7 +8,18 @@ interface Props {
 }
 
 const Traffic = ({ lights }: Props) => {
-    return <div></div>;
+    return (
+        <section className='w-[90%] flex flex-col items-center justify-between py-4 px-8 bg-white rounded-md'>
+            <div className='w-1/3 h-[35rem] bg-slate-400 rounded-lg flex flex-col justify-evenly items-center'>
+                {lights.map((light, index) => (
+                    <div
+                        key={light.color}
+                        className='border-2 bg-black rounded-full w-40 h-40'
+                    ></div>
+                ))}
+            </div>
+        </section>
+    );
 };
 
 export default Traffic;
