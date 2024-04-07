17ccc96e9d6b37458edf98db1787009525b62a68
Francis Yang <fy50167@gmail.com>
2024-04-06 22:03:42 -0700

    completed image carousel

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -33,13 +33,14 @@ const ImageCarousel = ({
     };
 
     const indicatorSet = (index: number) => {
+        console.log(slide, index);
         setSlide(index);
     };
 
     return (
         <div className='flex justify-center items-center w-[600px] h-auto relative overflow-hidden'>
             <BsArrowLeftCircleFill
-                className='absolute h-[2rem] w-[2rem] left-2 text-white hover:cursor-pointer'
+                className='absolute h-[2rem] w-[2rem] left-2 text-white hover:cursor-pointer z-10'
                 onClick={() => changeImage(-1)}
             />
             {images.map((item, index) => {
@@ -49,25 +50,28 @@ const ImageCarousel = ({
                         alt={item.alt}
                         width={600}
                         height={400}
-                        className={`rounded-md w-full h-full ${
-                            slide === index ? '' : 'hidden'
-                        }`}
+                        className={`rounded-md w-full h-full image`}
                         key={item.alt}
+                        style={{
+                            translate: `${-100 * (slide - 1)}%`,
+                        }}
                     />
                 );
             })}
             <BsArrowRightCircleFill
-                className='absolute h-[2rem] w-[2rem] right-2 text-white hover:cursor-pointer'
+                className='absolute h-[2rem] w-[2rem] right-2 text-white hover:cursor-pointer z-10'
                 onClick={() => changeImage(1)}
             />
-            <span className='absolute flex bottom-2 gap-2'>
+            <span className='absolute flex bottom-2 gap-2 items-center justify-center'>
                 {images.map((_, index) => {
                     return (
                         <button
                             onClick={() => indicatorSet(index)}
                             key={index}
                             className={`w-[10px] h-[10px] rounded-full bg-white ${
-                                slide === index ? 'bg-slate-700' : ''
+                                slide === index
+                                    ? 'w-[12px] h-[12px] bg-black'
+                                    : ''
                             }`}
                         ></button>
                     );
