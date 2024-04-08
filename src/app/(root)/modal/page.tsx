401e63fea3b29f9d572be3927ba5cd87bd60937f
Francis Yang <fy50167@gmail.com>
2024-04-07 22:02:44 -0700

    modal route

---
+++ src/app/(root)/modal/page.tsx
@@ -0,0 +1,9 @@
+import Modal from '@/components/Modal';
+
+export default function Page() {
+    return (
+        <div className='w-full bg-black min-h-[100vh] flex justify-center items-center'>
+            <Modal />
+        </div>
+    );
+}
