3aaf32a9c5448eab446a45f73d4353a0bcdbe2bd
Francis Yang <fy50167@gmail.com>
2024-04-10 23:43:10 -0700

    country selector

--- src/app/(root)/country/page.tsx
+++ src/app/(root)/country/page.tsx
@@ -2,7 +2,7 @@ import CountrySearch from '@/components/CountrySearch';
 
 export default function Page() {
     return (
-        <div className='w-full bg-black min-h-[100vh] flex justify-center items-center'>
+        <div className='w-full bg-black min-h-[100vh] flex flex-col justify-center items-center'>
             <CountrySearch />
         </div>
     );
--- src/components/CountrySearch.tsx
+++ src/components/CountrySearch.tsx
@@ -1,5 +1,82 @@
+'use client';
+
+import { useState, useEffect } from 'react';
+
+const BASE_URL: string = 'https://restcountries.com/v3.1';
+
+// all is /all, filter is /capital/{capital}
+
+const FILTERABLE_CAPITALS: string[] = [
+    'None',
+    'Tallinn',
+    'Helsinki',
+    'Stockholm',
+    'Oslo',
+    'Copenhagen',
+    'Reykjavik',
+];
+
+interface Country {
+    name: {
+        common: string;
+    };
+    capital: Array<string>;
+}
+
 const CountrySearch = () => {
-    return <div></div>;
+    const [countryData, setCountryData] = useState<Country[]>([]);
+    const [filter, setFilter] = useState<string>('None');
+
+    const onSelect = (e: React.ChangeEvent<HTMLSelectElement>) => {
+        console.log(e.target.value);
+        setFilter(e.target.value);
+    };
+
+    useEffect(() => {
+        const fetchCountryData = async () => {
+            if (filter === 'None') {
+                const response = await fetch(`${BASE_URL}/all`);
+                const res = await response.json();
+                setCountryData(res);
+            } else {
+                const response = await fetch(`${BASE_URL}/capital/${filter}`);
+                const res = await response.json();
+                setCountryData(res);
+            }
+        };
+
+        fetchCountryData();
+    }, [filter]);
+    return (
+        <>
+            <select className='w-1/3 mb-4' onChange={onSelect}>
+                {FILTERABLE_CAPITALS.map((capital, index) => (
+                    <option value={capital} key={capital}>
+                        {capital}
+                    </option>
+                ))}
+            </select>
+            <div className='bg-white w-[90%] max-h-[45rem] rounded-md overflow-auto p-4'>
+                {countryData.map((country, index) => {
+                    return (
+                        <div
+                            key={country.name.common}
+                            className={`w-full p-2 border-2 border-solid border-black flex flex-col items-start justify-evenly ${
+                                index !== countryData.length - 1 ? 'mb-2' : ''
+                            }`}
+                        >
+                            <h2 className='font-bold text-xl'>
+                                Name: {country.name.common}
+                            </h2>
+                            <p className='text-sm'>
+                                Capital: {country.capital}
+                            </p>
+                        </div>
+                    );
+                })}
+            </div>
+        </>
+    );
 };
 
 export default CountrySearch;
